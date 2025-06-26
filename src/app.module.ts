cbba51eaa5086f73b96b02155b11673954bbccfb
Saad Amir <saadamir070@gmail.com>
2025-06-26 23:57:20 +0500

    add log for db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,17 +11,20 @@ import { join } from 'path';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [join(process.cwd(), 'dist', '**', '*.entity{.ts,.js}')],
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
+      useFactory: (configService: ConfigService) => {
+        console.log('DB_HOST:', configService.get('DB_HOST')); // check output
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+          entities: [join(process.cwd(), 'dist', '**', '*.entity{.ts,.js}')],
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
     }),
   ],
   controllers: [AppController],
