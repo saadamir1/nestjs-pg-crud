5ed5d5ef215c70b4bc2b64fef4b433aaabc6c88a
Saad Amir <saadamir070@gmail.com>
2025-07-02 18:50:43 +0500

    docs(auth): add brief explanation for JwtAuthGuard behavior

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+// JWT strategy to validate and attach user info from token to request
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
